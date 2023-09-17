b15a873775df7e44bcb3a231fee31c6c2785f5e7
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-09-17 15:34:45 +0200

    added two modals

---
+++ modal2.js
@@ -0,0 +1,27 @@
+const modal2 = document.querySelector(".modal2");
+const overlay2 = document.querySelector(".overlay");
+
+const btnsOpenModal2 = document.querySelector(".show-modal2");
+
+const openModal2 = function () {
+  modal2.classList.remove("hidden");
+  overlay2.classList.remove("hidden");
+};
+
+const closeModal2 = function () {
+  modal2.classList.add("hidden");
+  overlay2.classList.add("hidden");
+};
+
+btnsOpenModal2.addEventListener("click", openModal2);
+console.log("button clicked");
+
+overlay2.addEventListener("click", closeModal2);
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal2.classList.contains("hidden")) {
+    closeModal2();
+  }
+});
