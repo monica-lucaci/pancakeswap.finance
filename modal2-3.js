5a4f8bc6a130846d02196251ac5e78da51b223af
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-09-19 11:17:20 +0200

    improved jscript

--- modal2-3.js
+++ modal2-3.js
@@ -1,153 +1,29 @@
-// const div1 = document.getElementById('sliderConnectDiv');
-// const div2 = document.getElementById('Web3WalletDiv');
-// const btn1 = document.getElementById('sliderConnectBtn');
-// const btn2 = document.getElementById('Web3WalletBtn');
+//modal2-> cambia div con bottoni sopra
 
+const div1 = document.getElementById("sliderConnectDiv");
+const div2 = document.getElementById("Web3WalletDiv");
+const btn1 = document.getElementById("sliderConnectBtn");
+const btn2 = document.getElementById("Web3WalletBtn");
 
-// // Function to toggle between div1 and div2 and interchange button styles
-// const toggleDivs = () => {
-//   if (div1.style.display === 'block' || div1.style.display === 'flex') {
-//     div1.style.display = 'none';
-//     div2.style.display = 'flex';
-//     btn1.classList.remove('modal2-slide1-btn1');
-//     btn1.classList.add('modal2-slide1-btn2');
-//     btn2.classList.remove('modal2-slide1-btn2');
-//     btn2.classList.add('modal2-slide1-btn1');
-//   } else {
-//     div1.style.display = 'flex';
-//     div2.style.display = 'none';
-//     btn1.classList.remove('modal2-slide1-btn2');
-//     btn1.classList.add('modal2-slide1-btn1');
-//     btn2.classList.remove('modal2-slide1-btn1');
-//     btn2.classList.add('modal2-slide1-btn2');
-//   }
-// };
-
-// // Add click event listeners to the buttons
-// btn1.addEventListener('click', toggleDivs);
-// btn2.addEventListener('click', toggleDivs);
-
-// const div1 = document.getElementById('sliderConnectDiv');
-// const div2 = document.getElementById('Web3WalletDiv');
-// const btn1 = document.getElementById('sliderConnectBtn');
-// const btn2 = document.getElementById('Web3WalletBtn');
-
-// // Function to toggle between div1 and div2 and interchange button styles
-// const toggleDivs = () => {
-//   if (div1.classList.contains('hidden')) {
-//     div1.classList.remove('hidden');
-//     div2.classList.add('hidden');
-//     btn1.classList.toggle('modal2-slide1-btn1');
-//     btn1.classList.toggle('modal2-slide1-btn2');
-//     btn2.classList.toggle('modal2-slide1-btn1');
-//     btn2.classList.toggle('modal2-slide1-btn2');
-//   } else {
-//     div1.classList.add('hidden');
-//     div2.classList.remove('hidden');
-//     btn1.classList.toggle('modal2-slide1-btn1');
-//     btn1.classList.toggle('modal2-slide1-btn2');
-//     btn2.classList.toggle('modal2-slide1-btn1');
-//     btn2.classList.toggle('modal2-slide1-btn2');
-//   }
-// };
-
-// // Add click event listeners to the buttons
-// btn1.addEventListener('click', toggleDivs);
-// btn2.addEventListener('click', toggleDivs);
-
-
-// const div1 = document.getElementById('sliderConnectDiv');
-// const div2 = document.getElementById('Web3WalletDiv');
-// const btn1 = document.getElementById('sliderConnectBtn');
-// const btn2 = document.getElementById('Web3WalletBtn');
-
-// // Function to toggle button styles
-// const toggleButtonStyles = () => {
-//   // Toggle the button classes
-//   btn1.classList.toggle('modal2-slide1-btn1');
-//   btn1.classList.toggle('modal2-slide1-btn2');
-//   btn2.classList.toggle('modal2-slide1-btn1');
-//   btn2.classList.toggle('modal2-slide1-btn2');
-
-//   // Check if btn2 should not show div1 and add/remove the class accordingly
-//   if (btn2.classList.contains('never-show-div1')) {
-//     div1.classList.add('hidden');
-//   } else {
-//     div1.classList.remove('hidden');
-//   }
-// };
-
-// // Add click event listeners to the buttons
-// btn1.addEventListener('click', toggleButtonStyles);
-// btn2.addEventListener('click', toggleButtonStyles);
-
-
-
-
-
-const div1 = document.getElementById('sliderConnectDiv');
-const div2 = document.getElementById('Web3WalletDiv');
-const btn1 = document.getElementById('sliderConnectBtn');
-const btn2 = document.getElementById('Web3WalletBtn');
-
-// Function to show div1 and hide div2, and toggle button styles
+// div1 compare  div2 scompare and intercambia stili btn
 const showDiv1 = () => {
-  div1.classList.remove('hidden');
-  div2.classList.add('hidden');
-  btn1.classList.add('modal2-slide1-btn1');
-  btn1.classList.remove('modal2-slide1-btn2');
-  btn2.classList.remove('modal2-slide1-btn1', 'never-show-div1');
-  btn2.classList.add('modal2-slide1-btn2');
+  div1.classList.remove("hidden");
+  div2.classList.add("hidden");
+  btn1.classList.add("modal2-slide1-btn1");
+  btn1.classList.remove("modal2-slide1-btn2");
+  btn2.classList.remove("modal2-slide1-btn1", "never-show-div1");
+  btn2.classList.add("modal2-slide1-btn2");
 };
 
-// Function to show div2 and hide div1, and toggle button styles
+// div2 compare  div1 scompare and intercambia stili btn
 const showDiv2 = () => {
-  div1.classList.add('hidden');
-  div2.classList.remove('hidden');
-  btn1.classList.remove('modal2-slide1-btn1');
-  btn1.classList.add('modal2-slide1-btn2');
-  btn2.classList.remove('modal2-slide1-btn2');
-  btn2.classList.add('modal2-slide1-btn1', 'never-show-div1');
+  div1.classList.add("hidden");
+  div2.classList.remove("hidden");
+  btn1.classList.remove("modal2-slide1-btn1");
+  btn1.classList.add("modal2-slide1-btn2");
+  btn2.classList.remove("modal2-slide1-btn2");
+  btn2.classList.add("modal2-slide1-btn1", "never-show-div1");
 };
 
-// Add click event listeners to the buttons
-btn1.addEventListener('click', showDiv1);
-btn2.addEventListener('click', showDiv2);
-
-
-
-
-//THIS
-//SHOULD
-//MAKE IT
-//AVAILABLE
-//WEBSITE WIDE
-
-// Function to toggle between divs and interchange button styles
-const toggleDivsAndStyles = (divToShow, divToHide, btnToShow, btnToHide) => {
-    return () => {
-      divToShow.classList.remove('hidden');
-      divToHide.classList.add('hidden');
-      btnToShow.classList.add('modal2-slide1-btn1');
-      btnToShow.classList.remove('modal2-slide1-btn2');
-      btnToHide.classList.remove('modal2-slide1-btn1');
-      btnToHide.classList.add('modal2-slide1-btn2');
-    };
-  };
-  
-  // Function to initialize the behavior for a specific section
-  const initializeSection = (sectionId, div1Id, div2Id, btn1Id, btn2Id) => {
-    const div1 = document.getElementById(div1Id);
-    const div2 = document.getElementById(div2Id);
-    const btn1 = document.getElementById(btn1Id);
-    const btn2 = document.getElementById(btn2Id);
-  
-    // Set up the event listeners for this section
-    btn1.addEventListener('click', toggleDivsAndStyles(div1, div2, btn1, btn2));
-    btn2.addEventListener('click', toggleDivsAndStyles(div2, div1, btn2, btn1));
-  };
-  
-  // Initialize the behavior for different sections on your website
-  initializeSection('section1', 'sliderConnectDiv', 'Web3WalletDiv', 'sliderConnectBtn', 'Web3WalletBtn');
-  initializeSection('section2', 'anotherDiv1', 'anotherDiv2', 'anotherBtn1', 'anotherBtn2');
-  
+btn1.addEventListener("click", showDiv1);
+btn2.addEventListener("click", showDiv2);
--- modal2.js
+++ modal2.js
@@ -1,65 +1,90 @@
-// const modal2 = document.querySelector('.modal2');
-// const overlay2 = document.querySelector('.overlay');
+//apri modal e chiudi settings//
 
-// const btnsOpenModal2 = document.querySelector('.show-modal2');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelector(".show-modal");
 
-// const openModal2 = function () {
-//   modal2.classList.remove('hidden');
-//   overlay2.classList.remove('hidden');
-// };
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
-// const closeModal2 = function () {
-//   modal2.classList.add('hidden');
-//   overlay2.classList.add('hidden');
-// };
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
+btnsOpenModal.addEventListener("click", openModal);
 
-// btnsOpenModal2.addEventListener('click', openModal2);
-// console.log("button clicked")
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-// overlay2.addEventListener('click', closeModal2);
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
 
-// document.addEventListener('keydown', function (e) {
-//   // console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
 
-//   if (e.key === 'Escape' && !modal2.classList.contains('hidden')) {
-//     closeModal2();
-//   }
-// });
+//apre il modal e l'overlay del connect button
 
-
-const modal2 = document.querySelector('.modal2'); 
-const overlay2 = document.querySelector('.overlay');
-const btnsOpenModal2 = document.querySelectorAll('.show-modal2');
-
-
+const modal2 = document.querySelector(".modal2");
+const btnsOpenModal2 = document.querySelectorAll(".show-modal2");
 
 const openModal2 = function () {
-  modal2.classList.remove('hidden');
-  overlay2.classList.remove('hidden');
+  modal2.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal2 = function () {
-  modal2.classList.add('hidden');
-  overlay2.classList.add('hidden');
+  modal2.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
 btnsOpenModal2.forEach((btn) => {
-  btn.addEventListener('click', openModal2);
+  btn.addEventListener("click", openModal2);
 });
 
-// closeModal2Buttons.forEach((btn) => {
-//   btn.addEventListener('click', closeModal2);
-// });
+overlay.addEventListener("click", closeModal2);
 
-overlay2.addEventListener('click', closeModal2);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal2.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal2.classList.contains("hidden")) {
     closeModal2();
   }
 });
 
+//more button modal2
 
+let itemCount = 8; // numero iniziale app
+const maxItems = 13; // numero totale di app
+const loadMoreBtn = document.getElementById("loadMoreBtn");
 
+function showMoreItems() {
+  const content = document.getElementById("modal2items");
+  const hiddenItems = document.querySelectorAll(".item.hidden");
+  const loadMoreBtn = document.getElementById("loadMoreBtn");
 
+  // controlla se ci sono piu app
+  if (itemCount < maxItems) {
+    // console.log(maxItems);
+    //  vedere 5 app  in piu
+    const itemsToShow = Math.min(maxItems - itemCount, 5);
+
+    // iterate app e farle vedere
+    hiddenItems.forEach((item, index) => {
+      if (index < itemsToShow) {
+        console.log(itemsToShow);
+        item.style.display = "flex"; // display gli elementi hidden
+        itemCount++;
+      }
+    });
+    // console.log(itemCount,maxItems);
+    if (itemCount >= maxItems) {
+      content.removeChild(loadMoreBtn);
+    }
+  }
+}
+
+loadMoreBtn.addEventListener("click", showMoreItems);
